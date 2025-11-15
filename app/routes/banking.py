ef343040999f3bad7ef1cf0f46a56741567a9b22
LorisjunodUnine <loris.junod@Unine.ch>
2025-11-15 19:24:19 +0100

    add routes/page for grouped ecritures creation

--- app/routes/banking.py
+++ app/routes/banking.py
@@ -4467,7 +4467,7 @@ def nouvelle_ecriture_from_transactions():
     
     # Récupérer les données pour les formulaires
     comptes = g.models.compte_model.get_all_accounts(current_user.id)
-    categories = g.models.categorie_model.get_all(current_user.id)
+    categories = g.models.categorie_comptable_model.get_all_categories(current_user.id)
     contacts = g.models.contact_model.get_all(current_user.id)
     
     return render_template('comptabilite/creer_ecritures_groupées.html',
