44caad5310f0481e01e343c06273f1a6b403c48e
LorisjunodUnine <loris.junod@Unine.ch>
2025-11-16 15:20:51 +0100

    add routes/page for grouped ecritures creation

--- app/routes/banking.py
+++ app/routes/banking.py
@@ -729,10 +729,10 @@ def banking_compte_evolution_echanges(compte_id):
     # Valeurs par défaut
     all_comptes = g.models.compte_model.get_all_accounts()
     comptes_cibles_possibles = [
-        {k: v for k, v in compte.items() if k in ['id', 'nom_compte']} # Garder uniquement les clés nécessaires
+        {'compte_id': compte['id'], 'nom_compte': compte['nom_compte']} 
         for compte in all_comptes
-            if compte['utilisateur_id'] == user_id and compte['id'] != compte_id # Exclure le compte source
-            ]
+        if compte['id'] != compte_id # Exclure le compte source
+        ]
     logging.info(f"banking XXX Comptes cibles {len(comptes_cibles_possibles)} possibles pour le compte {compte_id} (tous les comptes actifs de l'utilisateur sauf le compte source) : {comptes_cibles_possibles} ")
     date_debut = (date.today() - timedelta(days=90)).isoformat()
     date_fin = date.today().isoformat()
