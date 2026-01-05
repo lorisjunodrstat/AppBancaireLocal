ff126321c568dcd287b8192404c7fa4e6f8724d5
LorisjunodUnine <loris.junod@Unine.ch>
2026-01-05 16:19:48 +0100

    correction fonction login

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify, g
+from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify, g, current_app
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from ..models import Utilisateur
